391479d27e3759d2d6b2df9d3867d4c7c9217de0
shailendra sahu <shailendrasahu393@gmail.com>
2025-12-03 14:35:53 +0530

    First commit!!
    ​🪨📄✂️ C Rock-Paper-Scissors Game 
    
    ​This repository hosts a classic implementation of the Rock-Paper-Scissors game, developed entirely in the C programming language. It's a simple, console-based application designed to be lightweight, fast, and easy to compile and run on various platforms. Challenge the computer to a series of rounds and see if you can become the ultimate champion!
    
    ​✨ Features ​Classic Gameplay: Standard Rock-Paper-Scissors rules are implemented. ​Computer AI: A simple, non-predictive random number generation system is used for the computer's moves, ensuring fair and unpredictable matches. ​Score Tracking: The game tracks and displays the current score for both the player and the computer. ​Best-of-N Rounds: Players can choose how many rounds they wish to play (e.g., Best of 3, Best of 5) to determine the match winner. ​Clear Console Interface: Uses standard C printf and scanf functions for a clean and simple command-line user experience. ​Portable Code: Written using standard C libraries (stdio.h, stdlib.h, time.h), making it highly portable.

---
+++ RPS.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void main()
+{
+	int com;
+	char u,c,i='Y';
+	printf("Welcome to Rock - Paper - Scissors!\n");
+	printf("Instructions:-\n");
+	printf("Press R for Rock!\n");
+	printf("Press P for Paper!\n");
+	printf("Press S for Scissors!\n");
+	while(i='Y')
+	{
+		printf("\nPlease select your move : ");
+		scanf("%s",&u);
+		fflush(stdin);
+		com= rand()%3;
+		if(com==0)
+		{
+			c='R';
+		}
+		if(com==1)
+		{
+			c='P';
+		}
+		if(com==2)
+		{
+			c='S';
+		}
+		if((u=='R')&&(c=='S')||(u=='S')&&(c=='P')||(u=='P')&&(c=='R'))
+		{
+			printf("Computer's move : %c",c);
+			printf("\nYou Win!");
+		}
+		if((u=='R')&&(c=='P')||(u=='S')&&(c=='R')||(u=='P')&&(c=='S'))
+		{
+			printf("Computer's move : %c",c);
+			printf("\nYou Lose!");
+		}
+		if(u==c)
+		{
+			printf("\nComputer's move : %c",c);
+			printf("\nMatch tie!");
+		}
+	
+		printf("\nDo you want to play again? (Y,N) : ");
+		fflush(stdin);
+		scanf("%s",&i);
+		if(i=='N')
+		{
+			break;
+		}
+	}
+	printf("\nThankyou for Playing!");
+}
